097d567126037398fa38529bdd8974d6c5c54548
Decland.h-at-857577032516 email = RTqDW0Yu6vZts2t3gCMuts4CnrgtyQ5gEQ1LYJsTI3A= <708879@cognizant.com>
2020-07-06 12:27:48 +0100

    pushing code for further dev

--- src/app/exampage/exampage.component.ts
+++ src/app/exampage/exampage.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from  '@angular/router'
-
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import {Router} from  '@angular/router';
+// 1 added new for monitoring - starts//
+import {Subject, Observable} from 'rxjs';
+import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
+import {HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+// 1 added new for monitoring - ends//
 
 @Component({
   selector: 'app-exampage',
@@ -9,13 +14,79 @@ import {Router} from  '@angular/router'
 })
 export class ExampageComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,private http:HttpClient) { } //2 added for HTTPClient
 
-  ngOnInit() {
+  // 3 added new for monitoring - starts//
+  public pictureTaken = new EventEmitter<WebcamImage>();
+  public showWebcam = true;
+  public multipleWebcamsAvailable = false;
+  public deviceId: string;
+  public returnval: string;
+  public intervalHandle: any;
+  public videoOptions: MediaTrackConstraints = {
+    // width: {ideal: 1024},
+    // height: {ideal: 576}
+  };
+
+  public errors: WebcamInitError[] = [];
+  public webcamImage: WebcamImage = null;
+  private trigger: Subject<void> = new Subject<void>();
+  loginResponse: string
+  // 3 added new for monitoring - ends//
+
+  public handleImage(webcamImage: WebcamImage): void {
+    console.info('received webcam image', webcamImage);
+    this.webcamImage = webcamImage;
+
   }
 
+
+  // 4 added new for monitoring - starts//
+
+  public ngOnInit(): void{
+
+    WebcamUtil.getAvailableVideoInputs()
+      .then((mediaDevices: MediaDeviceInfo[]) => {
+        this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length > 1;
+      }).then(()=>{
+       this.postcontinue();
+      });
+
+}
+
+public postcontinue(): void{
+  this.intervalHandle =
+    setInterval(()=> {
+      this.trigger.next();
+      let headers = new HttpHeaders({
+      'Content-Type':  'application/json'
+      });
+    this.http.post("http://localhost:5000/faceMonitering", { img: this.webcamImage.imageAsBase64 }, {headers }).subscribe((res:any) => {
+    //this.loginResponse = JSON.stringify(res)
+      //  alert(this.loginResponse);
+      if (res.valid === false){
+        alert(res.error);
+      }
+      console.log(res);
+    })}, 5000); // currently interval is set for every 5 seconds.
+};
+
+public handleInitError(error: WebcamInitError): void {
+  this.errors.push(error);
+}
+
+
+
+
+public get triggerObservable(): Observable<void> {
+      return this.trigger.asObservable();
+    }
+
+// 4 added new for monitoring - ends//
+
 completeExam(){
-   this.router.navigate(['examcomplete']);
+  clearInterval(this.intervalHandle);
+  this.router.navigate(['examcomplete']);
 
 
   }
